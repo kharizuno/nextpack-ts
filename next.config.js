3e01ce16722d51675b36a4f08e9eaf0f988949a3
kharizuno <kharizuno48@gmail.com>
2022-08-21 19:45:04 +0700

    update

--- next.config.js
+++ next.config.js
@@ -4,18 +4,23 @@ const nextConfig = {
 	swcMinify: true,
 }
 
+const dotenv = require('dotenv');
+const path = require('path');
+
 module.exports = (phase, { defaultConfig }) => {
-  let develop = (process.env.BUILD_ENV !== undefined) ? process.env.BUILD_ENV : process.env.NODE_ENV;
+	let develop = (process.env.BUILD_ENV !== undefined) ? process.env.BUILD_ENV : process.env.NODE_ENV;
 	develop = (develop === 'development') ? 'staging' : develop;
-  
+
 	let production = (develop === 'production') ? false : true;
+	dotenv.config({ path: path.resolve(__dirname, 'config/.env.' + develop) });
 
 	return {
 		...nextConfig,
 		productionBrowserSourceMaps: production,
 		env: {
-			URL_WEB: 'http://localhost:5100/',
-			URL_API: 'https://graphql.anilist.co/'
+			URL_WEB: process.env.URL_WEB,
+			URL_API: process.env.URL_API,
+			API_ENCRYPT: process.env.API_ENCRYPT
 		}
 	}
 }
